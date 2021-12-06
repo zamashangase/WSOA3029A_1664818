df456c0d3bb68372d04ae7bb882b4096d955943b
zamashangase <1664818@students.wits.ac.za>
2021-12-06 19:29:35 +0200

    circle d3 code

--- circle.js
+++ circle.js
@@ -3,7 +3,7 @@ var width = 800
 var height = 800
 var data = [10, 25, 50, 65, 80]
 var colors = ['#03045e', '#0077b6', '#00b4d8', '#90e0ef', '#caf0f8']
-var svg = d3.select('body')
+var svg = d3.select('#body')
             .append('svg')
             .attr('width', width)
             .attr('height', height);
